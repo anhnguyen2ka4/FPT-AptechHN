ad754e09fc3aa5b37efbe2a236c0024393d5a0a2
anhnguyen2ka4 <anhnguyen2ka4@gmail.com>
2023-11-27 14:10:29 +0700

    Create Input.jsx

---
+++ AptechHN/W3school-react/react-js/src/example-in-class/icons/Input.jsx
@@ -0,0 +1,27 @@
+import ErrorIcon from "./Error";
+import Valid from "./Valid";
+export default function Input(props) {
+    return (
+        <div
+            className={`input-group ${
+              !props.isValid && props.isTouched
+                ? "invalid"
+                : `${props.isValid && props.isTouched ? "valid" : ""}`}`}>
+            <label htmlFor={props.name}>{props.label}</label>
+            <input
+                className="input"
+                name={props.name}
+                type={props.type}
+                placeholder={props.placeholder}
+                value={props.value}
+                onChange={(e) => props.onChange(e)} />
+            <span className="input-error" > {` ${
+                !props.isValid && props.isTouched ? props.errorMsg : ""
+                }`}</span >
+            <div className="input__icon">
+                {props.isTouched && !props.isValid && <ErrorIcon />} 
+                {props.isTouched && props.isValid && <Valid />}
+            </div>
+        </div >
+    );
+}
