f0b17fa675b1d0905b28b57a976c01863adad7cf
anhnguyen2ka4 <anhnguyen2ka4@gmail.com>
2023-12-13 23:42:40 +0700

    Create table_customers.sql

---
+++ AptechHN/W3School_SQL/table_customers.sql
@@ -0,0 +1,20 @@
+USE Master
+GO
+IF EXISTS (SELECT * FROM sys.databases WHERE Name='Customers_table')
+DROP DATABASE Customers_table
+GO
+CREATE DATABASE Customers_table
+GO
+USE Customers_table
+GO
+
+-- create table
+CREATE TABLE customers(
+	customer_id INT IDENTITY (1,1) PRIMARY KEY,
+	customer_name VARCHAR (50) NOT NULL,
+	contact_name VARCHAR (25) NOT NULL,
+	c_address VARCHAR (255) NOT NULL,
+	city VARCHAR (50) NOT NULL,
+	postal_code VARCHAR (50),
+	country VARCHAR (25) NOT NULL
+	);
