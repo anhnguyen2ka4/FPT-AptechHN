7738de06a6187a53d3c0b22fff611c6cb692eaeb
anhnguyen2ka4 <anhnguyen2ka4@gmail.com>
2023-12-16 21:21:07 +0700

    Update insert-table_customers.sql

--- AptechHN/W3School_SQL/insert-table_customers.sql
+++ AptechHN/W3School_SQL/insert-table_customers.sql
@@ -1,13 +1,16 @@
-USE Customers_table
+USE CustomersTable
 GO
 
-SET IDENTITY_INSERT customers ON;
+SET IDENTITY_INSERT Customers ON;
 
-INSERT INTO customers(customer_id,customer_name,contact_name,c_address,city,postal_code,country) VALUES(1,'Alfres Futterkiste','Maria Ander','Obere Str. 57','Berlin'  ,'12250','Germany')
-INSERT INTO customers(customer_id,customer_name,contact_name,c_address,city,postal_code,country) VALUES(2,'Anbert Anhxtanh'   ,'Anhxtanh'   ,'Wall Street'  ,'New York','65404','American')
-INSERT INTO customers(customer_id,customer_name,contact_name,c_address,city,postal_code,country) VALUES(3,'Ho Chi Minh' ,'Bac Ho'   ,'Nam Dan'  ,'Nghe An','100000','Viet Nam')
-INSERT INTO customers(customer_id,customer_name,contact_name,c_address,city,postal_code,country) VALUES(4,'Lep-ton-xtoi'   ,'Lep'   ,'Babylon Garden'  ,'Ai Captian','404','Ai Cap')
-INSERT INTO customers(customer_id,customer_name,contact_name,c_address,city,postal_code,country) VALUES(5,'Error and Bug'   ,'Fix Bug'   ,'Programming'  ,'Code','99999','Database')
-INSERT INTO customers(customer_id,customer_name,contact_name,c_address,city,postal_code,country) VALUES(6,'Roman'   ,'kaka'   ,'Sideres'  ,'Shang hai','686868','China')
+INSERT INTO Customers(CustomerID,CustomerName,ContactName,CustomerAddress,City,PostalCode,Country) VALUES(1,'Alfres Futterkiste','Maria Ander','Obere Str. 57','Berlin'  ,'12250','Germany')
+INSERT INTO Customers(CustomerID,CustomerName,ContactName,CustomerAddress,City,PostalCode,Country) VALUES(2,'Anbert Anhxtanh'   ,'Anhxtanh'   ,'Wall Street'  ,'New York','65404','American')
+INSERT INTO Customers(CustomerID,CustomerName,ContactName,CustomerAddress,City,PostalCode,Country) VALUES(3,'Ho Chi Minh' ,'Bac Ho'   ,'Nam Dan'  ,'Nghe An','100000','Viet Nam')
+INSERT INTO Customers(CustomerID,CustomerName,ContactName,CustomerAddress,City,PostalCode,Country) VALUES(4,'Lep-ton-xtoi'   ,'Lep'   ,'Babylon Garden'  ,'Ai Captian','404','Ai Cap')
+INSERT INTO Customers(CustomerID,CustomerName,ContactName,CustomerAddress,City,PostalCode,Country) VALUES(5,'Error and Bug'   ,'Fix Bug'   ,'Programming'  ,'Code','99999','Database')
+INSERT INTO Customers(CustomerID,CustomerName,ContactName,CustomerAddress,City,PostalCode,Country) VALUES(6,'Roman'   ,'kaka'   ,'Sideres'  ,'Shang hai','686868','China')
+INSERT INTO Customers(CustomerID,CustomerName,ContactName,CustomerAddress,City,PostalCode,Country) VALUES(7,'Ronandol Fake','Real','Wall Street','New York','65404','American')
+INSERT INTO Customers(CustomerID,CustomerName,ContactName,CustomerAddress,City,PostalCode,Country) VALUES(8,'Messi Normal','Fake','So 8 Ton That Thuyet','Ha Noi','100000','Viet Nam')
+INSERT INTO Customers(CustomerID,CustomerName,ContactName,CustomerAddress,City,PostalCode,Country) VALUES(9,'Naruto','GoKu','8 Dragon Ball','Moonlight','9999','Viet Nam')
 
-SET IDENTITY_INSERT customers OFF;
+SET IDENTITY_INSERT Customers OFF;
