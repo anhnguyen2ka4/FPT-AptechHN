acfd4f405b00ec854dc0dc53d4c68455a430a785
anhnguyen2ka4 <anhnguyen2ka4@gmail.com>
2023-12-24 23:54:30 +0700

    Update Syntax.php

--- AptechHN/W3School_PHP/Syntax/Syntax.php
+++ AptechHN/W3School_PHP/Syntax/Syntax.php
@@ -2,3 +2,19 @@
 $txt = 'PHP';
 echo "I love $txt!";
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Syntax</title>
+</head>
+<body>
+    <h1>My second PHP</h1>
+    <?php 
+    echo "Welcome to PHP."
+    ?>
+</body>
+</html>
