7d33efa77914a04b1eea88de45dd0e129140aaf3
anhnguyen2ka4 <anhnguyen2ka4@gmail.com>
2023-12-26 10:47:01 +0700

    Create multi-line_comment.php

---
+++ AptechHN/W3School_PHP/Comments/multi-line_comment.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Multi-line Comment</title>
+</head>
+<body>
+    <?php
+    /*
+    The next statement will
+    print a welcome message
+    */
+    echo "Welcome Home";
+    ?>
+</body>
+</html>
