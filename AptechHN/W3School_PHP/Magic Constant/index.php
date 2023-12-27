2814290da8a87c42260a6d6c0d420c2ada2640dc
anhnguyen2ka4 <anhnguyen2ka4@gmail.com>
2023-12-27 23:17:32 +0700

    Create index.php

---
+++ AptechHN/W3School_PHP/Magic Constant/index.php
@@ -0,0 +1,4 @@
+<?php
+echo "<h1>PHP Predefined Constants</h1>";
+echo "PHP has nine predefined constants that change value depending on where they are used and therefor they are called magic constants.These magic constants are written with a double underscore at the start and the end, except for the ClassName::class constant.";
+?>
