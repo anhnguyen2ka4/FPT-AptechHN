7f0827342f44a2d20cbc1373557686c4d2492cf3
anhnguyen2ka4 <anhnguyen2ka4@gmail.com>
2023-12-31 10:46:05 +0700

    Create slicing.php

---
+++ AptechHN/W3School_PHP/String/slicing.php
@@ -0,0 +1,11 @@
+<?php
+$a = "Welcome to TechWiz5 2024!";
+echo substr($a, 6, 13); //start charater:6 
+                        //length of string:13
+echo "<br>";
+echo substr($a, 11);
+echo "<br>";
+echo substr($a, -14, 8);
+echo "<br>";
+echo substr($a, 2, -1);
+?>
