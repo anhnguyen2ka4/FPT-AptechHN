37d56c1ad29bb7839ed5c75d395130bb7c40a74e
anhnguyen2ka4 <anhnguyen2ka4@gmail.com>
2023-12-31 23:05:06 +0700

    Create escapeCharacter.php

---
+++ AptechHN/W3School_PHP/String/escapeCharacter.php
@@ -0,0 +1,9 @@
+<?php
+$a = "We are the so-called \"Vikings\" from the north.";
+//  \' ... \' , \r, \n, ...
+// $a = 'We are the so-called "Vikings" from the north.' . "\n";
+echo "\n".$a;
+echo "<br>";
+$b = "\100\145\154";
+echo $b;
+?>
