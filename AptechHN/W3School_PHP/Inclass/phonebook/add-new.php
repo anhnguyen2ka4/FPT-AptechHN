0256f766dee18595ab8550e7392359c472e1bb7d
Nguyen Duc Anh <anhnguyen2ka4@gmail.com>
2024-01-20 10:36:51 +0700

    Update add-new.php

--- AptechHN/W3School_PHP/Inclass/phonebook/add-new.php
+++ AptechHN/W3School_PHP/Inclass/phonebook/add-new.php
@@ -37,12 +37,12 @@ if (isset($_POST["submit"])) {
    <!-- Font Awesome -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-   <title>PHP CRUD Application</title>
+   <title>ADD Contact</title>
 </head>
 
 <body>
    <nav class="navbar navbar-light justify-content-center fs-3 mb-5" style="background-color: #00ff5573;">
-      PHP Complete CRUD Application
+      Add Contact
    </nav>
 
    <div class="container">
