ed983a34ce3ce49adf0d92350884edfbfa4e8ddc
Nguyen Duc Anh <anhnguyen2ka4@gmail.com>
2024-01-20 10:51:10 +0700

    Create phonebook.sql

---
+++ AptechHN/W3School_PHP/Inclass/phonebook/phonebook.sql
@@ -0,0 +1,11 @@
+create database phpcrud
+use phpcrud
+
+CREATE TABLE crud (
+  id int(255) NOT NULL AUTO_INCREMENT,
+  first_name varchar(255) NOT NULL,
+  last_name varchar(255) NOT NULL,
+  phone varchar(255) NOT NULL,
+  gender varchar(255) NOT NULL,
+  PRIMARY KEY (id)
+)
