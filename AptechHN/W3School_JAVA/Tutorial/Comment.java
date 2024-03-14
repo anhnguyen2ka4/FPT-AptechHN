451c40a8b80b349711a22ce4c79e4360e49a74d6
Nguyen Duc Anh <anhnguyen2ka4@gmail.com>
2024-03-14 22:58:19 +0700

    Create Comment.java

---
+++ AptechHN/W3School_JAVA/Tutorial/Comment.java
@@ -0,0 +1,9 @@
+public class Comment {
+    public static void main(String[] args) {
+        // This is comment
+        /*
+          This is also comment
+         */
+        System.out.println("Comment above this line.");
+    }
+}
