f875c50f25aeccc37c12a7869a0de894a55b33fe
Nguyen Duc Anh <anhnguyen2ka4@gmail.com>
2024-03-14 22:58:22 +0700

    Create Output.java

---
+++ AptechHN/W3School_JAVA/Tutorial/Output.java
@@ -0,0 +1,8 @@
+public class Output {
+    public static void main(String[] args) {
+        System.out.print("I'm from Viet Nam.");// print text
+        System.out.print("I'm ");
+        System.out.print(2*10);// print numbers
+        System.out.print(" years old.");
+    }
+}
